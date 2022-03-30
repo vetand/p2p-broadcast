0eeb7bb57ddb0dea73cbab078573fcd4ab3fc7f1
rekrutik <rekrutik@gmail.com>
2022-03-30 14:42:55 +0300

    Switch to plain message

--- app/tcp_transport.py
+++ app/tcp_transport.py
@@ -19,7 +19,7 @@ class TCPTransport(Transport):
         return TCPTransport(d)
 
     async def send_message(self, peer, message):
-        message = { 'message': message.decode() }
+        message = {'message': message}
         if peer.transports.get('tcp') is not None:
             try:
                 wrapped_message = {"protocol": "p2p-1.0", "payload": message}
